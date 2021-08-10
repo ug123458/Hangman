2c9ffe083f50e353bd2d20241cc667c75381db8a
ug123458 <ug123458@gmail.com>
2021-08-10 20:28:51 +0530

    added functionality and other things

--- src/App.js
+++ src/App.js
@@ -1,12 +1,57 @@
+import React, { useState, useEffect } from 'react'
 import './App.css'
 import Header from './components/Header'
 import Figure from './components/Figure'
+import WrongLetters from './components/WrongLetters'
+import Word from './components/Word'
+
+const words = ['application', 'programming', 'interface', 'wizard']
+
+let selectedWord = words[Math.floor(Math.random() * words.length)]
+
+let playable = true
+
+const correctLetters = []
+const wrongLetters = []
+
 function App() {
+  const [playable, setplayable] = useState(true)
+  const [correctLetters, setcorrectLetters] = useState([])
+  const [wrongLetters, setwrongLetters] = useState([])
+
+  useEffect(() => {
+    const handleKeydown = (event) => {
+      const { key, keyCode } = event
+      if (playable && keyCode >= 65 && keyCode <= 90) {
+        const letter = key.toLowerCase()
+
+        if (selectedWord.includes(letter)) {
+          if (!correctLetters.includes(letter)) {
+            setcorrectLetters((correctLetters) => [...correctLetters, letter])
+          } else {
+            //showNotification();
+          }
+        } else {
+          if (!wrongLetters.includes(letter)) {
+            setwrongLetters((wrongLetters) => [...wrongLetters, letter])
+          } else {
+            //showNotification();
+          }
+        }
+      }
+    }
+    window.addEventListener('keydown', handleKeydown)
+
+    return () => window.removeEventListener('keydown', handleKeydown)
+  }, [correctLetters, wrongLetters, playable])
+
   return (
     <>
       <Header />
       <div className='game-container'>
-        <Figure />
+        <Figure wrongLetters={wrongLetters} />
+        <WrongLetters wrongLetters={wrongLetters} />
+        <Word selectedWord={selectedWord} correctLetters={correctLetters} />
       </div>
     </>
   )
--- src/components/Figure.js
+++ src/components/Figure.js
@@ -1,6 +1,8 @@
 import React from 'react'
 
-const Figure = () => {
+const Figure = ({ wrongLetters }) => {
+  const errors = wrongLetters.length
+
   return (
     <svg height='250' width='200' className='figure-container'>
       <line x1='60' y1='20' x2='140' y2='20' />
@@ -8,12 +10,12 @@ const Figure = () => {
       <line x1='60' y1='20' x2='60' y2='230' />
       <line x1='20' y1='230' x2='100' y2='230' />
 
-      <circle cx='140' cy='70' r='20' className='figure-part' />
-      <line x1='140' y1='90' x2='140' y2='150' className='figure-part' />
-      <line x1='140' y1='120' x2='120' y2='100' className='figure-part' />
-      <line x1='140' y1='120' x2='160' y2='100' className='figure-part' />
-      <line x1='140' y1='150' x2='120' y2='180' className='figure-part' />
-      <line x1='140' y1='150' x2='160' y2='180' className='figure-part' />
+      {errors > 0 && <circle cx='140' cy='70' r='20' />}
+      {errors > 1 && <line x1='140' y1='90' x2='140' y2='150' />}
+      {errors > 2 && <line x1='140' y1='120' x2='120' y2='100' />}
+      {errors > 3 && <line x1='140' y1='120' x2='160' y2='100' />}
+      {errors > 4 && <line x1='140' y1='150' x2='120' y2='180' />}
+      {errors > 5 && <line x1='140' y1='150' x2='160' y2='180' />}
     </svg>
   )
 }
